69a811e97b0c0616336e5be868c05737835125da
Aleksandar Targov <aleksandar.targov@sap.com>
2024-01-23 22:36:05 +0200

    make manifest

--- components/controller/api/v1alpha1/groupversion_info.go
+++ components/controller/api/v1alpha1/groupversion_info.go
@@ -15,8 +15,8 @@ limitations under the License.
 */
 
 // Package v1alpha1 contains API Schema definitions for the op v1alpha1 API group
-//+kubebuilder:object:generate=true
-//+groupName=func.controller
+// +kubebuilder:object:generate=true
+// +groupName=func.controller
 package v1alpha1
 
 import (
